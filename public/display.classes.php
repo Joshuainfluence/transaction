5504de7ad2d657af87459a493c636ef1b0cbed00
certifiedyrn <omoriolej@gmail.com>
2025-06-29 06:58:55 +0100

    modified display classes query to use tracking number and not status

--- public/display.classes.php
+++ public/display.classes.php
@@ -1,11 +1,28 @@
 <?php
 require_once __DIR__ . "/../config/dbh.php";
+require_once __DIR__ ."/../config/session.php";
+
 class Display extends Dbh{
-    protected function displayDetails($status): array
+    // protected function displayDetails($status): array
+    // {
+    //     $sql = "SELECT * FROM information WHERE status = ?";
+    //     $stmt = $this->connection()->prepare(query: $sql);
+    //     if (!$stmt->execute([$status])) {
+    //         $stmt = null;
+    //         exit();
+    //     }
+    //     $details = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    //     return $details;
+    // }
+
+
+    // WORKING WITH TRACKING NUMBER
+
+    protected function displayDetails($tracking_number): array
     {
-        $sql = "SELECT * FROM information WHERE status = ?";
+        $sql = "SELECT * FROM information WHERE tracking_number = ?";
         $stmt = $this->connection()->prepare(query: $sql);
-        if (!$stmt->execute([$status])) {
+        if (!$stmt->execute([$tracking_number])) {
             $stmt = null;
             exit();
         }
