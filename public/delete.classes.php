f06f11a1f6ce0c341418d4abc1145010a89a7eda
certifiedyrn <omoriolej@gmail.com>
2025-06-29 06:11:27 +0100

    delete transaction class

---
+++ public/delete.classes.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__. "/../config/dbh.php";
+class Delete extends Dbh{
+    protected function deleteTransaction($id){
+        $sql = "DELETE * FROM information WHERE id = :id";
+        // $stmt = $this->connection()->prepare($sql);
+        $statement = $this->connection()->prepare($sql);
+
+        $statement->bindParam(':id', $id);
+
+        if (!$statement->execute()) {
+            $statement = null;
+            exit();
+        }
+        return null;
+    }
+}
