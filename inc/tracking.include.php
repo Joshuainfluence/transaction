ec0705f61360cc852965ba5422580b4e6f62a6f1
certifiedyrn <omoriolej@gmail.com>
2025-06-29 06:51:45 +0100

    storing tracking number in session after successful validation

--- inc/tracking.include.php
+++ inc/tracking.include.php
@@ -1,9 +1,12 @@
 <?php 
+require_once __DIR__. "/../config/session.php";
 
 $tracking_number = $_GET['tracking_number'];
 require_once __DIR__. "/../public/tracking.classes.php";
 require_once __DIR__. "/../public/tracking.contr.php";
 $track = new TrackingContr($tracking_number);
 $track->trackingValidate();
+
+$_SESSION['tracking_number'] = $tracking_number;
 header("Location: ../index.php?success: user found");
 
