65d401eec3bdbe6b7650f3445c072ab10e0c8268
certifiedyrn <omoriolej@gmail.com>
2025-06-29 06:34:15 +0100

    tracking class

---
+++ public/tracking.classes.php
@@ -0,0 +1,24 @@
+<?php 
+require_once __DIR__. "/../config/dbh.php";
+class Tracking_classes extends Dbh{
+    private function set_message($type, $message){
+        return $_SESSION[$type] = $message;
+    }
+    protected function validateTracking($tracking_number)
+    {
+        $sql = "SELECT * FROM information WHERE tracking_number = ?";
+        $stmt = $this->connection()->prepare($sql);
+        if (!$stmt->execute([$tracking_number])) {
+            $stmt = null;
+            exit();
+        }
+
+        if ($stmt->rowCount() === 0) {
+           $stmt = null;
+           $this->set_message("error", "User not found");
+           header("Location: ../index.php");
+        }
+        $details = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $details;
+    }
+}
