6024cf09218fad5b6e22b4aa0c198626aebf4d5b
unknown <joshuajulius2030@gmail.com>
2025-06-30 13:26:00 +0100

    admin controller function

---
+++ public/admin.contr.php
@@ -0,0 +1,33 @@
+<?php
+require_once __DIR__ . "/../config/dbh.php";
+require_once __DIR__. "/../config/session.php";
+
+class AdminContr extends Dbh
+{
+    private $username;
+    private $password;
+
+    private $error;
+
+    public function __construct($username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    private function set_message($type, $message)
+    {
+        $_SESSION[$type] = $message;
+    }
+
+    public function loginAdmin()
+    {
+        if ($this->username !== "" && $this->password !== "") {
+            $this->set_message("error", "Admin Login incorrect!");
+            header("Location: ../admin/index.php");
+        }
+
+        header("Location: ../admin/form.php");
+         $this->set_message("success", "Login successful");
+    }
+}
