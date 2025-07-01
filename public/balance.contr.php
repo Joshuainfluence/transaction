9b63a14a8a7950872f89b700beca201df5f9eee1
unknown <joshuajulius2030@gmail.com>
2025-07-02 00:50:56 +0100

    balance controller

---
+++ public/balance.contr.php
@@ -0,0 +1,83 @@
+<?php
+
+require_once __DIR__ . "/../config/session.php";
+// require_once __DIR__."/../public/signup.classes.php";
+class InsertContr extends Insert
+{
+    private $bank_name;
+    private $account_number;
+    private $account_type;
+    private $balance_amount;
+
+    private $details;
+
+    private $passcode;
+
+    private $status;
+    
+
+
+    
+    //property to store validation error
+    //setting it to public to have access to it from the index file
+
+    public $error;
+    // public $verification;
+
+
+    public function __construct($bank_name, $account_number, $account_type, $balance_amount, $details, $passcode)
+    {
+        $this->bank_name = $bank_name;
+        $this->account_number = $account_number;
+        $this->account_type = $account_type;
+        $this->balance_amount = $balance_amount;
+     
+        $this->details = $details;
+        $this->passcode = $passcode;
+        
+
+        
+    }
+
+    private function emptyInput()
+    {
+        $result = 0;
+        if (empty($this->bank_name) || empty($this->account_number) || empty($this->account_type) || empty($this->balance_amount) || empty($this->details) ) {
+            $result = true;
+        } else {
+            $result = false;
+        }
+        return $result;
+    }
+
+
+
+
+    private function set_message($type, $message)
+    {
+        $_SESSION[$type] = $message;
+    }
+
+
+    public function insertDetails()
+    {
+        if ($this->emptyInput() == true) {
+            $this->set_message("error", "Fields cannot be empty");
+            header("Location: ../admin/form.php?error=emptyfields");
+            exit();
+        }
+
+
+        
+        $this->set_message("success", "Registration successful");
+
+        $this->Insert($this->bank_name, $this->account_number, $this->account_type, $this->balance_amount, $this->details, $this->passcode);
+    }
+
+
+
+ 
+
+
+
+}
