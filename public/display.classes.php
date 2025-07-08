b0a2fb18957bb5dbc4ec6012b82e730e65f2c70a
unknown <joshuajulius2030@gmail.com>
2025-07-08 15:58:09 +0100

    cleaning up

--- public/display.classes.php
+++ public/display.classes.php
@@ -3,19 +3,6 @@ require_once __DIR__ . "/../config/dbh.php";
 require_once __DIR__ ."/../config/session.php";
 
 class Display extends Dbh{
-    // protected function displayDetails($status): array
-    // {
-    //     $sql = "SELECT * FROM information WHERE status = ?";
-    //     $stmt = $this->connection()->prepare(query: $sql);
-    //     if (!$stmt->execute([$status])) {
-    //         $stmt = null;
-    //         exit();
-    //     }
-    //     $details = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    //     return $details;
-    // }
-
-
     // WORKING WITH TRACKING NUMBER
 
     protected function displayDetails($tracking_number): array
