7c3adcd8aced923b0c95e23b817dfa58f7568b7a
unknown <joshuajulius2030@gmail.com>
2025-07-09 13:55:52 +0100

    title

--- balance.php
+++ balance.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Transaction</title>
+    <title>Balance</title>
     <link rel="stylesheet" href="style.css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,300,regular,500,600,700,800,900,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic"
